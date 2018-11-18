5c6c0a087c9efd2cd68d72550eb3473b3ba2bfc2
cocet33000 <cocet33000@gmail.com>
2018-11-19 03:17:15 +0900

    updata

--- make_cipher_pkl.py
+++ make_cipher_pkl.py
@@ -1,40 +1,41 @@
-import matplotlib.pyplot as plt
-import pickle 
+import pickle
+
 
 def split_n(text, n):
-    return [ '0x' + text[i*n:i*n+n] for i in xrange(len(text)/n) ]
+    return ['0x' + text[i*n:i*n+n] for i in xrange(len(text)/n)]
+
 
 data = []
 
-with open('file/aes_tv_0000001-0005000_power.csv',mode = 'r') as f:
+with open('file/aes_tv_0000001-0005000_power.csv', mode='r') as f:
     for line in f:
         line = line.strip()
         line = line.split(',')
         data.append(list(map(int, line)))
     print('complete 1-1')
 
-with open('file/aes_tv_0005001-0010000_power.csv',mode = 'r') as f:
+with open('file/aes_tv_0005001-0010000_power.csv', mode='r') as f:
     for line in f:
         line = line.strip()
         line = line.split(',')
         data.append(list(map(int, line)))
     print('complete 1-2')
 
-with open('aes.pkl',mode = 'wb') as f:
+with open('aes.pkl', mode='wb') as f:
     print('writing 1')
-    pickle.dump(data,f)
+    pickle.dump(data, f)
 
 data = []
 
-with open('file/cipher.txt',mode = 'r') as f:
+with open('file/cipher.txt', mode='r') as f:
     for line in f:
         line = line.strip()
         line = line[2:]
-        li = split_n(line,4)
-        li = [int(n,0) for n in li]
+        li = split_n(line, 4)
+        li = [int(n, 0) for n in li]
         data.append(li)
     print('complete 2')
 
-with open('cipher.pkl',mode = 'wb') as f:
-    pickle.dump(data,f)
+with open('cipher.pkl', mode='wb') as f:
+    pickle.dump(data, f)
     print('writed 2')
--- profile.py
+++ profile.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pyAES.myAES import AES_unroll 
+from pyAES.myAES import AES_unroll
 from Control.utility import *
 import numpy as np
 import pickle
-import yaml
-import os
-
 
 aobj = AES_unroll()
 hamming_distance = {}
@@ -15,19 +12,21 @@ intermediate_values = {}
 num = 0
 
 
-def cal_hamming(A): 
+def cal_hamming(A):
     hamming = []
     a = []
     nine = A.data[9]
-    ten = A.data[10] 
+    ten = A.data[10]
     for i in range(len(nine)):
-        a.append(format(nine[i] ^ ten[i],'08x'))
+        a.append(format(nine[i] ^ ten[i], '08x'))
     for data in a:
-        hamming.extend([str(bin(int('0x' + (i+j),0))).count('1') for (i,j) in zip(data[::2],data[1::2])])
+        hamming.extend([str(bin(int('0x' + (i+j), 0))).count('1')
+                        for (i, j) in zip(data[::2], data[1::2])])
     return(hamming)
 
-# load cipher text 
-with open('./pkl/cipher.pkl','rb') as f:
+
+# load cipher text
+with open('./pkl/cipher.pkl', 'rb') as f:
     cipher = pickle.load(f)
 
 # cipher text loop
@@ -35,31 +34,28 @@ for ct in cipher:
     # guess_key loop (256)
     for partial_key in range(256):
         key = [partial_key*2**8 + partial_key for l in range(8)]
-        aobj.keyExpansion(key) 
-        # change key of 10R    
-        aobj.subkey[40:44] = [int('0x' + format(partial_key,'02x')*4,0) for loop in range(4)]
-        aobj.decrypt(ct) #decrypt
-        if(partial_key == 0 ):
+        aobj.keyExpansion(key)
+        # change key of 10R
+        aobj.subkey[40:44] = [
+            int('0x' + format(partial_key, '02x')*4, 0) for loop in range(4)]
+        aobj.decrypt(ct)        # decrypt
+        if(partial_key == 0):
             cpa_tmp = np.array([cal_hamming(aobj)])
         else:
-            cpa_tmp = np.append(cpa_tmp,[cal_hamming(aobj)],axis=0)
+            cpa_tmp = np.append(cpa_tmp, [cal_hamming(aobj)], axis=0)
     if num == 0:
         cpa = np.array([cpa_tmp])
     else:
-        cpa = np.append(cpa,np.array([cpa_tmp]),axis=0)
+        cpa = np.append(cpa, np.array([cpa_tmp]), axis=0)
     num += 1
     print(num)
 
-#list for shft rows
-lis = [ 0,5,10,15,
-        4,9,14,3,
-        8,13,2,7,
-        12,1,6,11
-      ]
+# list for shft rows
+lis = [0, 5, 10, 15, 4, 9, 14, 3,  8, 13, 2, 7, 12, 1, 6, 11]
 
 # write file
 for num in range(16):
-    with open('./pkl/b' + str(num) + '.pkl',mode = 'wb') as f:
-        data = cpa[:,:,lis[num]]
+    with open('./pkl/b' + str(num) + '.pkl', mode='wb') as f:
+        data = cpa[:, :, lis[num]]
         print(data.shape)
-        pickle.dump(data,f)
+        pickle.dump(data, f)
